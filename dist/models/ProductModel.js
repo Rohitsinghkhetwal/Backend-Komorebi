42a4ce1e4961860b8918f0e85e2fa0c7baeae099
rohit <raj0eshsing0909@gmail.com>
2023-04-29 01:33:36 +0530

    Initial commit

---
+++ dist/models/ProductModel.js
@@ -0,0 +1,29 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var mongoose_1 = __importDefault(require("mongoose"));
+var ProductSchema = new mongoose_1.default.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+}, {
+    timestamps: true
+});
+var Product = mongoose_1.default.model("products", ProductSchema);
+exports.default = Product;
+//# sourceMappingURL=ProductModel.js.map
