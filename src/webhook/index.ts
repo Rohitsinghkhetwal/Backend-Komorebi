ff5c3eb3f41940bff8433c5b35a6dccfdd7bbf27
rohit <raj0eshsing0909@gmail.com>
2023-04-29 13:01:19 +0530

    ft: webhook moved

--- src/webhook/index.ts
+++ src/webhook/index.ts
@@ -16,7 +16,19 @@ export const webhookHandler = async(request: Request, response: Response) => {
             sig,
             process.env.STRIPE_WEB_HOOK_SECRET
         )
-        if(event.type === "payment_intent.created") {
+        if(event.type === "charge.succeeded") {
+            const charge = event.data.object as stripe.Charge
+            const order = await Order.findOne({
+                paymentIntentId: charge.payment_intent,
+
+            })
+            if(order) {
+                order.paymentStatus ="paid"
+                order.paymentDetail = charge
+                await order.save()
+            }
+        }
+        else if(event.type === "charge.failed") {
             const charge = event.data.object as stripe.Charge
             const order = await Order.findOne({
                 paymentIntentId: charge.payment_intent,
